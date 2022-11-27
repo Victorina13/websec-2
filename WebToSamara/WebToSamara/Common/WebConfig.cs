41482639aa7f5cd07241d28341c2a2f20a44f157
vika lo <victoria.tchekushkina@yandex.ru>
2022-11-27 17:50:41 +0400

    add/edit config

---
+++ WebToSamara/WebToSamara/Common/WebConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace WebToSamara.Common
+{
+    public class WebConfig
+    {
+        public string ClientId { get; set; }
+        public string Os { get; set; }
+        public string Secret_key { get; set; }
+        public string RequestUrl { get; set; }
+
+        public WebConfig(string clientId, string os, string secret_key, string requestUrl)
+        {
+            ClientId = clientId;
+            Os = os;
+            Secret_key = secret_key;
+            RequestUrl = requestUrl;
+        }
+    }
+}
--- WebToSamara/WebToSamara/Controllers/MainController.cs
+++ WebToSamara/WebToSamara/Controllers/MainController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebToSamara.Models;
+using System.Configuration;
+using WebToSamara.Common;
+using Newtonsoft.Json;
 
 namespace WebToSamara.Controllers
 {
     public class MainController : Controller
     {
+        private List<string> _methods = new List<string>() { "getFirstArrivalToStop", "getRouteSchedule", "getTransportPosition" };
+        
+        public Stops StopsObj { get; set; }
+        
+        public Routes RoutesObj { get; set; }
+        
         private readonly ILogger<MainController> _logger;
 
-        public MainController(ILogger<MainController> logger)
+        private readonly WebConfig Configuration;
+
+        public MainController(ILogger<MainController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            StopsObj = new Stops();
+            RoutesObj = new Routes();
+            var config = configuration.GetSection("Configuration");
+            Configuration = new WebConfig(config.GetValue<string>("clientId"), config.GetValue<string>("os"), config.GetValue<string>("secret_key"), config.GetValue<string>("requestUrl"));
         }
 
         public IActionResult Index()

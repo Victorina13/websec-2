7e742672936aeaafe97e346ff4b9f49a867671f2
vika lo <victoria.tchekushkina@yandex.ru>
2022-11-27 17:05:58 +0400

    blank for the controller

---
+++ WebToSamara/WebToSamara/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebToSamara.Models;
+
+namespace WebToSamara.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Views/Main/Index.cshtml");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}

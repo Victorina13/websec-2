8db1f2d0dde4b21f2206ed0c1a3e0c37e5261544
vika lo <victoria.tchekushkina@yandex.ru>
2022-11-27 18:01:52 +0400

    add method for build request body

--- WebToSamara/WebToSamara/Controllers/MainController.cs
+++ WebToSamara/WebToSamara/Controllers/MainController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using WebToSamara.Models;
-using System.Configuration;
 using WebToSamara.Common;
-using Newtonsoft.Json;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace WebToSamara.Controllers
 {
@@ -26,6 +25,31 @@ namespace WebToSamara.Controllers
             RoutesObj = new Routes();
             var config = configuration.GetSection("Configuration");
             Configuration = new WebConfig(config.GetValue<string>("clientId"), config.GetValue<string>("os"), config.GetValue<string>("secret_key"), config.GetValue<string>("requestUrl"));
+        }
+
+        public FormUrlEncodedContent GetRequestString(string methodName, string KS_ID, string HULLNO, string KR_ID)
+        {
+            string? message = methodName == _methods[0]
+                    ? $"{{\"method\":\"{methodName}\", \"KS_ID\":{KS_ID},\"COUNT\":{20}}}"
+                    : methodName == _methods[1]
+                        ? $"{{\"method\":\"{methodName}\", \"KR_ID\":{KR_ID},\"day\":{DateTime.Now:dd.MM.yyyy}}}"
+                        : methodName == _methods[2]
+                            ? $"{{\"method\":\"{methodName}\", \"HULLNO\":{HULLNO}}}"
+                            : String.Empty;
+
+
+            byte[] hash = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes($"{message}{Configuration.Secret_key}"));
+            string resHash = string.Join("",hash.Select(b => b.ToString("x2")).ToList());
+            
+            var body = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("message", message),
+                new KeyValuePair<string, string>("os", Configuration.Os),
+                new KeyValuePair<string, string>("clientId", Configuration.ClientId),
+                new KeyValuePair<string, string>("authKey", resHash)
+            };
+
+            return new FormUrlEncodedContent(body);
         }
 
         public IActionResult Index()
